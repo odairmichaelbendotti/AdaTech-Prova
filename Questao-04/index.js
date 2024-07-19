b368d86248f0af143cb23d549a592410996f9d7a
Odair Michael Bendotti <odairmichaelbendotti@gmail.com>
2024-07-19 20:37:38 -0300

    Finalização questão 3

---
+++ Questao-04/index.js
@@ -0,0 +1,12 @@
+function contar(){
+    for (let i = 1; i <= 100; i++) {    
+        if ( i % 5 === 0 ){
+            //console.log(`${i} - Ada`)
+            console.log(`Ada`)
+        } else {
+            console.log(i)
+        }
+    }
+}
+
+contar()
